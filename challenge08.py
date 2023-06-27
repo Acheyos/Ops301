4e26f933a35916604e299d8b3a06bc8b2ad3ad21
Acheyos <acheyos@protonmail.com>
2023-06-26 21:39:10 -0400

    first commit

---
+++ challenge08.py
@@ -0,0 +1,9 @@
+#Python
+
+groceries = ["apple", "banana", "cherry", "durian", "grape", "melon", "mango", "orange", "pineapple", "strawberry"]
+
+
+print(groceries[3])
+print(groceries[5:])
+groceries[6] = "onion"
+print(groceries)
